4daed40c8008d24f661389e1d424265c1c908901
Your Name <rachelbarkanic@Rachels-MacBook-Air.local>
2023-03-17 17:25:38 -0700

    moving along

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
-    <div className='expense-item'>
-        <div>March 28 2021</div>
+    <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$249.67</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
             </div>
-            </div>
+            </Card>
         );
 }
 
